b625f91b21c64286fcd09422b00387ca4335bc1b
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-12-01 21:31:37 -0800

    commit tree 76f27ddeefea9c944d90929536e57f02d581d6f5

---
+++ index.js
@@ -0,0 +1,7 @@
+const divide = function (a = 2000, b = 100) {
+    return a / b;
+}
+
+const square = parameter1 => parameter1 * parameter1;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
